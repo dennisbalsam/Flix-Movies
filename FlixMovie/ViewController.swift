bdb59b56e0f0291cb339b7947002d5f2e9ad06c4
Dennis Krupitsky <d.krupitsky@gmail.com>
2020-02-19 20:59:13 -0600

    Initial commit

---
+++ FlixMovie/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FlixMovie
+//
+//  Created by user165734 on 2/19/20.
+//  Copyright © 2020 user165734. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
