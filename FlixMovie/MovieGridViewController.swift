e62101e15498d1da406107fd14668b4495b021d8
Dennis Krupitsky <d.krupitsky@gmail.com>
2020-02-28 18:22:34 -0600

    adding second page

---
+++ FlixMovie/MovieGridViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MovieGridViewController.swift
+//  FlixMovie
+//
+//  Created by user165734 on 2/28/20.
+//  Copyright © 2020 user165734. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
